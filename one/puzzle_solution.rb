f93275a2b8877c08f10388029fc7588dfa449602
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-12-01 15:14:48 -0700

    Refactor for readability

--- one/puzzle_solution.rb
+++ one/puzzle_solution.rb
@@ -1,63 +1,20 @@
 def calibration(file)
   sum = 0
   File.readlines(file).each do |line|
-    puts find_num(line)
-    sum += find_num(line)
+    sum += find_nums(line)
   end
 
   return sum
 end
 
-def find_num(string)
+def find_nums(string)
   num = ""
-  first_hash = {}
-  second_hash = {}
   nums = [["one", 1], ["two", 2], ["three", 3], ["four", 4], ["five", 5], ["six", 6], ["seven", 7], ["eight", 8], ["nine", 9]]
+  first_word = find_first_word(string, nums)
+  last_word = find_last_word(string, nums)
 
-  nums.each do |word|
-    if !string.index(word[0]).nil?
-      first_hash[word[1]] = string.index(word[0])
-    end
-  end
-
-  nums.each do |word|
-    if !string.index(word[0]).nil?
-      second_hash[word[1]] = string.rindex(word[0])
-    end
-  end
-
-  first_word = first_hash.min_by do |key, value|
-    value
-  end
-
-  last_word = second_hash.max_by do |key, value|
-    value
-  end
-
-  first_num = []
-  last_num = []
-
-  string.chars.each_with_index do |char, index|
-    if char == "0"
-      first_num = [char, index]
-      break
-    elsif char.to_i != 0
-      first_num = [char, index]
-      break
-    end
-  end
-
-  length = string.length - 1
-  string.reverse.chars.each_with_index do |char, index|
-    index = length - index
-    if char == "0"
-      last_num = [char, index]
-      break
-    elsif char.to_i != 0
-      last_num = [char, index]
-      break
-    end
-  end
+  first_num = find_first_num(string)
+  last_num = find_last_num(string)
 
   first_num[1] = string.length if first_num[1].nil?
   last_num[1] = 0 if last_num[1].nil?
@@ -75,6 +32,67 @@ def find_num(string)
   end
 
   return num.to_i
+end
+
+def find_first_word(string, nums)
+  hash = {}
+
+  nums.each do |word|
+    if !string.index(word[0]).nil?
+      hash[word[1]] = string.index(word[0])
+    end
+  end
+
+  hash.min_by do |key, value|
+    value
+  end
+end
+
+def find_last_word(string, nums)
+  hash = {}
+
+  nums.each do |word|
+    if !string.index(word[0]).nil?
+      hash[word[1]] = string.rindex(word[0])
+    end
+  end
+
+  last_word = hash.max_by do |key, value|
+    value
+  end
+end
+
+def find_first_num(string)
+  first_num = []
+
+  string.chars.each_with_index do |char, index|
+    if char == "0"
+      first_num = [char, index]
+      break
+    elsif char.to_i != 0
+      first_num = [char, index]
+      break
+    end
+  end
+
+  first_num
+end
+
+def find_last_num(string)
+  last_num = []
+  length = string.length - 1
+  string.reverse.chars.each_with_index do |char, index|
+    index = length - index
+    if char == "0"
+      last_num = [char, index]
+      break
+    elsif char.to_i != 0
+      last_num = [char, index]
+      break
+    end
+  end
+
+  last_num
 end
 
 puts calibration('./one/data_one.txt')
